3a865e8518cb5591ebcf29304585d4f6c1ef90ed
Sunwoo Park <sunwoo.park@lignex1.com>
2023-04-17 17:36:22 +0900

    MSS.cpp 수정 중

--- MSS/MSS.cpp
+++ MSS/MSS.cpp
@@ -1,14 +1,6 @@
-﻿// MSS.cpp : 정적 라이브러리를 위한 함수를 정의합니다.
-//
-
-#include "pch.h"
+﻿#include "pch.h"
 #include "framework.h"
 #include <cmath>
-
-// TODO: 라이브러리 함수의 예제입니다.
-void fnMSS()
-{
-}
 
 class MssPositionDeliverer {
 	void SendMssPosition(double, double);
