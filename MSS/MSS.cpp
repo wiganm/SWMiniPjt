1291a2fe08d06a4504c4be7802c0bacf40cf754d
Sunwoo Park <sunwoo.park@lignex1.com>
2023-04-17 17:05:03 +0900

    Revert "Modify MssPositionDeliverer, MssSimulator"
    This reverts commit 60f3806ecaa233b5ef9a089bc62e2310bbfcb574.

--- MSS/MSS.cpp
+++ MSS/MSS.cpp
@@ -15,66 +15,24 @@ class MssPositionDeliverer {
 };
 
 void MssPositionDeliverer::SendMssPosition(double MssPositionX, double MssPositionY) {
-	//???
+	return 0;
 }
 
 class MssSimulator {
 public:
-	double InitX;
-	double InitY;
-	double currentX;
-	double currentY;
-
 	double MssPositionX;
 	double MssPositionY;
 	int SimulationState;
-private:
-	void Initialize(double, double);
-	void Start(int);
-	void Stop(int);
-	void Move(double, double);
+	void Start();
+	void Stop();
+	void Move();
 	void UpdateMssPosition();
 	void UpdateSimulationState();
 };
 
-void MssSimulator::Initialize(double MssStartX, double MssStartY) {
-	//Set start position
-	double InitX = MssStartX;
-	double InitY = MssStartY;
-	//Set current position
-	double currentX = InitX;
-	double currentY = InitY;
-}
-
-void MssSimulator::Start(int SimulationState) {
-	//Initialize();
-	while (SimulationState == 1) {
-		Move();
-	}
-}
-
-void MssSimulator::Stop(int SimulationState) {
-	while (SimulationState == 0) {
-		//???
-	}
-}
-
 void MssSimulator::Move(double vectorX, double vectorY) {
-	//Get next position
-	currentX += vectorX;
-	currentY += vectorY;
-}
-
-void MssSimulator::UpdateMssPosition() {
-	MssPositionX = currentX;
-	MssPositionY = currentY;
-}
-
-void MssSimulator::UpdateSimulationState() {
-	if (SimulationState == 0) { 
-
-	}
-	if (SimulationState == 1) {
-
-	}
+	//double tempX = MssPositionX;
+	//double tempY = MssPositionY;
+	MssPositionX += vectorX;
+	MssPositionY += vectorY;
 }
