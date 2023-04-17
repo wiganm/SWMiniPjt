0e7a6b49ffff9ad8f8e3d03cbd4fa0558cead494
Sunwoo Park <sunwoo.park@lignex1.com>
2023-04-17 14:24:21 +0900

    Initialize MssPositionDeliverer

--- MSS/MSS.cpp
+++ MSS/MSS.cpp
@@ -3,8 +3,17 @@
 
 #include "pch.h"
 #include "framework.h"
+#include <cmath>
 
 // TODO: 라이브러리 함수의 예제입니다.
 void fnMSS()
 {
+}
+
+class MssPositionDeliverer {
+	void SendMssPosition(double, double);
+};
+
+void MssPositionDeliverer::SendMssPosition(double MssPositionX, double MssPositionY) {
+	return 0;
 }
